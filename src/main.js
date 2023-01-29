2329d0066dff79ab3c4a66cc6528884b56575cd6
muxihong <17608507695@163.com>
2023-01-29 18:02:52 +0800

    登录登出功能初步实现

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+//导入全局样式表
+import './assets/css/global.css'
 
+import axios from "axios"
+Vue.prototype.$http=axios  //挂载
+axios.defaults.baseURL='https://www.liulongbin.top:8888/api/private/v1/'  //配置请求的根路径
 
-//手动配置
+//手动配置ElementUI
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
-Vue.use(Router)
 
 
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,35 @@
 import Vue from "vue";
 import Router from 'vue-router'
 Vue.use(Router)
+
+const router = new Router({
+    routes:[
+        {
+          path:'/',
+          redirect:'/login'  //重定向
+        },
+        {
+            path:'/login',
+            meta:'登录界面',
+            component:()=>import('../components/Login')  //懒加载方式
+        },
+        {
+            path:'/home',
+            meta:'主页',
+            component:()=>import('../components/Home')
+        }
+    ]
+})
+//挂在路由导航守卫
+router.beforeEach((to,from,next)=>{
+    //to 将要访问的路径
+    //from 代表从哪个路劲跳转过来的
+    //next 是一个函数，表示放行    next() 放行  next('/login') 强制跳转
+    if (to.path==='/login') return next()
+    //获取token
+    const tokenStr = window.sessionStorage.getItem('token')
+    if (!tokenStr) return next('/login')
+    next()
+})
+
+export default router
